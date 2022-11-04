52a3fdce8888a72c6744bb8d56df44a22d6ab8d6
Sacoya Adams <Sacoyaengineer@gmail.com>
2022-11-04 18:30:06 -0400

    commit tree 7ef11f4e41fff090ff1c319772af41796918238e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+    console.log("This saturday, I want to roller-skate!")
+}
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(speacial = 'Sacoya, '){
+    console.log("You are ! where")
+}
